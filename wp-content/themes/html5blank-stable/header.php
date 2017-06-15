3d4ac490e52af86c30ca337a56bbebfa5dd66ffd
chuckflr <cflores057@icloud.com>
2017-06-15 16:10:45 -0700

    adding fonts and styling

--- wp-content/themes/html5blank-stable/header.php
+++ wp-content/themes/html5blank-stable/header.php
@@ -33,16 +33,13 @@
 
 					<!-- logo -->
 					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
+						<a href="<?php echo home_url(); ?>"></a>
 					</div>
 					<!-- /logo -->
 
 					<!-- nav -->
 					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
+						
 					</nav>
 					<!-- /nav -->
 
